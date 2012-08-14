5d8c35297c469c0f938c0e198103d4483590dc90
Yegor Bugayenko <yegor@tpc2.com>
2012-08-14 21:01:52 +0200

    #39: implemented and tested, test fails

---
+++ src/main/java/com/jcabi/aspects/RetryOnFailure.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2012, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.aspects;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Retry the method in case of exception.
+ *
+ * <p>Add this annotation to the method you want to log. Any exception thrown
+ * out of it will be logged through {@link com.jcabi.log.Logger}.
+ *
+ * @author Yegor Bugayenko (yegor@jcabi.com)
+ * @version $Id$
+ * @since 0.1.10
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RetryOnFailure {
+
+    /**
+     * How many times to retry.
+     */
+    int attempts() default 3;
+
+    /**
+     * Delay between attempts, in milliseconds.
+     */
+    long delay() default 50;
+
+    /**
+     * When to retry (in case of what exception types).
+     */
+    Class<? extends Exception>[] types() default {};
+
+}
---
+++ src/main/java/com/jcabi/aspects/aj/Repeater.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2012, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.aspects.aj;
+
+import com.jcabi.aspects.RetryOnFailure;
+import com.jcabi.log.Logger;
+import java.util.concurrent.TimeUnit;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+/**
+ * Repeat execution in case of exception.
+ *
+ * @author Yegor Bugayenko (yegor@jcabi.com)
+ * @version $Id$
+ * @since 0.1.10
+ * @see com.jcabi.aspects.RetryOnFailure
+ */
+@Aspect
+public final class Repeater {
+
+    /**
+     * Catch exception and re-call the method.
+     * @param point Joint point
+     * @return The result of call
+     * @throws Throwable If something goes wrong inside
+     * @checkstyle IllegalThrows (5 lines)
+     * @checkstyle LineLength (4 lines)
+     */
+    @Around("execution(* * (..)) && @annotation(com.jcabi.aspects.RetryOnFailure)")
+    @SuppressWarnings("PMD.AvoidCatchingThrowable")
+    public Object wrap(final ProceedingJoinPoint point) throws Throwable {
+        final RetryOnFailure rof = MethodSignature.class
+            .cast(point.getSignature())
+            .getMethod()
+            .getAnnotation(RetryOnFailure.class);
+        int attempt = 0;
+        while (true) {
+            try {
+                return point.proceed();
+            // @checkstyle IllegalCatch (1 line)
+            } catch (Throwable ex) {
+                ++attempt;
+                Logger.warn(
+                    this,
+                    "attempt #%d %[exception]s",
+                    attempt,
+                    ex
+                );
+                if (attempt > rof.attempts()) {
+                    throw ex;
+                }
+                TimeUnit.MILLISECONDS.sleep(rof.delay() * attempt);
+            }
+        }
+    }
+
+}
