cdb0ed23443c4f5c617d78678225f9b16e0d03a8
Yegor Bugayenko <yegor@tpc2.com>
2012-11-29 20:18:57 +0100

    #101: skeleton created, works fine

---
+++ src/main/java/com/jcabi/aspects/aj/MethodLogger.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2012, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.aspects.aj;
+
+import com.jcabi.log.Logger;
+import java.lang.reflect.Method;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+/**
+ * Logs method calls.
+ *
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ * @since 0.8
+ */
+@Aspect
+public final class MethodLogger {
+
+    /**
+     * Catch exception and re-call the method.
+     * @param point Joint point
+     * @return The result of call
+     * @throws Throwable If something goes wrong inside
+     * @checkstyle IllegalThrows (5 lines)
+     */
+    @Around("execution(* * (..)) && @annotation(com.jcabi.aspects.Loggable)")
+    @SuppressWarnings("PMD.AvoidCatchingThrowable")
+    public Object wrap(final ProceedingJoinPoint point) throws Throwable {
+        final long start = System.nanoTime();
+        final Object result = point.proceed();
+        final Method method = MethodSignature.class.cast(
+            point.getSignature()
+        ).getMethod();
+        final StringBuilder log = new StringBuilder();
+        log.append('#').append(method.getName()).append('(');
+        final Object[] args = point.getArgs();
+        for (int pos = 0; pos < args.length; ++pos) {
+            if (pos > 0) {
+                log.append(", ");
+            }
+            log.append(args[pos]);
+        }
+        log.append("): ");
+        log.append(result);
+        log.append(Logger.format(" in %[nano]s", System.nanoTime() - start));
+        Logger.info(method.getDeclaringClass(), log.toString());
+        return result;
+    }
+
+}
---
+++ src/test/java/com/jcabi/aspects/aj/MethodLoggerTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2012, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.aspects.aj;
+
+import java.lang.reflect.Method;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Test case for {@link MethodLogger}.
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ */
+public final class MethodLoggerTest {
+
+    /**
+     * MethodLogger can log simple calls.
+     * @throws Throwable If something goes wrong
+     * @checkstyle IllegalThrows (5 lines)
+     */
+    @Test
+    public void logsSimpleCall() throws Throwable {
+        this.call(new Object[] {null});
+    }
+
+    /**
+     * Call it with the provided params.
+     * @param args The args
+     * @throws Throwable If something goes wrong
+     * @checkstyle IllegalThrows (5 lines)
+     */
+    private void call(final Object[] args) throws Throwable {
+        final MethodLogger logger = new MethodLogger();
+        final Method method = MethodLoggerTest.Foo.class
+            .getMethod("revert", String.class);
+        final MethodSignature sig = Mockito.mock(MethodSignature.class);
+        Mockito.doReturn(method).when(sig).getMethod();
+        final ProceedingJoinPoint point =
+            Mockito.mock(ProceedingJoinPoint.class);
+        Mockito.doReturn(sig).when(point).getSignature();
+        Mockito.doReturn(args).when(point).getArgs();
+        logger.wrap(point);
+    }
+
+    /**
+     * Dummy class, for tests above.
+     */
+    private static final class Foo {
+        /**
+         * Revert string.
+         * @param text Some text
+         * @return Reverted text
+         */
+        public String revert(final String text) {
+            return new StringBuffer(text).reverse().toString();
+        }
+    }
+
+}
