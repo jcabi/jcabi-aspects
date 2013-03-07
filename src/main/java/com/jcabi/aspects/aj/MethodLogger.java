6a9411cae9ad4c0e77cc9abc6a9ed68723e9abe3
Yegor Bugayenko <yegor@tpc2.com>
2013-03-07 20:05:25 +0100

    #157: annotation fixed to avoid runtime tests

--- src/main/java/com/jcabi/aspects/aj/MethodLogger.java
+++ src/main/java/com/jcabi/aspects/aj/MethodLogger.java
@@ -58,7 +58,7 @@ public final class MethodLogger {
      * @checkstyle IllegalThrows (5 lines)
      * @checkstyle LineLength (3 lines)
      */
-    @Around("(execution(* *(..)) || initialization(*.new(..))) && @this(com.jcabi.aspects.Loggable)")
+    @Around("(execution(* (@com.jcabi.aspects.Loggable *).*(..)) || initialization((@com.jcabi.aspects.Loggable *).new(..)))")
     public Object wrapClass(final ProceedingJoinPoint point) throws Throwable {
         final Method method =
             MethodSignature.class.cast(point.getSignature()).getMethod();
