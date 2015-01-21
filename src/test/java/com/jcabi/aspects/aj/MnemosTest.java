c78a31a7b3689e24b788533e2fb27285fff20fb3
oruam85 <oruam85@gmail.com>
2015-01-21 12:10:12 -0430

    121:  Add handling of int, long, float, double, char and boolean arrays.

--- src/test/java/com/jcabi/aspects/aj/MnemosTest.java
+++ src/test/java/com/jcabi/aspects/aj/MnemosTest.java
@@ -86,4 +86,123 @@ public final class MnemosTest {
         );
     }
 
+    /**
+     * Mnemos can build a string from an int array.
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void buildsTextFromIntArray() throws Exception {
+        final Object[][] pairs = new Object[][] {
+            new Object[] {new int[0], "[]"},
+            new Object[] {new int[] {1}, "[1]"},
+            new Object[] {new int[] {1, 2, 3}, "[1, 2, 3]"},
+        };
+        for (final Object[] pair : pairs) {
+            MatcherAssert.assertThat(
+                Mnemos.toText(pair[0], false, false),
+                Matchers.equalTo(pair[1].toString())
+            );
+        }
+    }
+
+    /**
+     * Mnemos can build a string from an long array.
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void buildsTextFromLongArray() throws Exception {
+        final Object[][] pairs = new Object[][] {
+            new Object[] {new long[0], "[]"},
+            new Object[] {new long[] {1L}, "[1]"},
+            new Object[] {new long[] {1L, 2L, 3L}, "[1, 2, 3]"}
+        };
+        for (final Object[] pair : pairs) {
+            MatcherAssert.assertThat(
+                Mnemos.toText(pair[0], false, false),
+                Matchers.equalTo(pair[1].toString())
+            );
+        }
+    }
+
+    /**
+     * Mnemos can build a string from an float array.
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void buildsTextFromFloatArray() throws Exception {
+        final Object[][] pairs = new Object[][] {
+            new Object[] {new float[0], "[]"},
+            new Object[] {new float[] {1.01f}, "[1.01]"},
+            new Object[] {
+                new float[] {1.01f, 2.02f, 3.03f},
+                "[1.01, 2.02, 3.03]"
+            }
+        };
+        for (final Object[] pair : pairs) {
+            MatcherAssert.assertThat(
+                Mnemos.toText(pair[0], false, false),
+                Matchers.equalTo(pair[1].toString())
+            );
+        }
+    }
+
+    /**
+     * Mnemos can build a string from an double array.
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void buildsTextFromDoubleArray() throws Exception {
+        final Object[][] pairs = new Object[][] {
+            new Object[] {new double[0], "[]"},
+            new Object[] {new double[] {1.01}, "[1.01]"},
+            new Object[] {new double[] {1.01, 2.02, 3.03}, "[1.01, 2.02, 3.03]"}
+        };
+        for (final Object[] pair : pairs) {
+            MatcherAssert.assertThat(
+                Mnemos.toText(pair[0], false, false),
+                Matchers.equalTo(pair[1].toString())
+            );
+        }
+    }
+
+    /**
+     * Mnemos can build a string from an char array.
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void buildsTextFromCharArray() throws Exception {
+        final Object[][] pairs = new Object[][] {
+            new Object[] {new char[0], "[]"},
+            new Object[] {new char[] {'a'}, "[a]"},
+            new Object[] {new char[] {'a', 'b', 'c'}, "[a, b, c]"}
+        };
+        for (final Object[] pair : pairs) {
+            MatcherAssert.assertThat(
+                Mnemos.toText(pair[0], false, false),
+                Matchers.equalTo(pair[1].toString())
+            );
+        }
+    }
+
+    /**
+     * Mnemos can build a string from an boolean array.
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void buildsTextFromBooleanArray() throws Exception {
+        final Object[][] pairs = new Object[][] {
+            new Object[] {new boolean[0], "[]"},
+            new Object[] {new boolean[] {true}, "[true]"},
+            new Object[] {
+                new boolean[] {true, false, false},
+                "[true, false, false]"
+            }
+        };
+        for (final Object[] pair : pairs) {
+            MatcherAssert.assertThat(
+                Mnemos.toText(pair[0], false, false),
+                Matchers.equalTo(pair[1].toString())
+            );
+        }
+    }
 }
