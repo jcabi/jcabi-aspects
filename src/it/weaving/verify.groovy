3128558491a10c70a321c76cde2accdfa5ca5503
Yegor Bugayenko <yegor256@gmail.com>
2025-02-07 12:36:36 +0300

    new copyright holder

--- src/it/weaving/verify.groovy
+++ src/it/weaving/verify.groovy
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012-2025, jcabi.com
+ * Copyright (c) 2012-2025 Yegor Bugayenko
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
