cfc1214401ebc1349c6210fff787d804576476c8
uchitsa <uchitsa@ya.ru>
2026-01-19 16:48:20 +0300

    #421 fix errors

--- src/test/java/com/jcabi/aspects/aj/MethodSchedulerTest.java
+++ src/test/java/com/jcabi/aspects/aj/MethodSchedulerTest.java
@@ -46,8 +46,23 @@ final class MethodSchedulerTest {
         }
     }
 
+    @Test
+    void shortRunningTaskShouldBeAllowedToFinish() throws Exception {
+        final MethodSchedulerTest.ShortRun target = new MethodSchedulerTest.ShortRun();
+        TimeUnit.SECONDS.sleep(5);
+        target.close();
+        MatcherAssert.assertThat("should be true", target.finished, Matchers.equalTo(true));
+    }
+
+    @Test
+    void interruptLongRunningTask() throws Exception {
+        final MethodSchedulerTest.LongRun target = new MethodSchedulerTest.LongRun();
+        target.close();
+        MatcherAssert.assertThat("should be false", target.finished, Matchers.equalTo(false));
+    }
+
     /**
-     * Long running task.
+     * Long-running task.
      * @since 0.7.22
      */
     @ScheduleWithFixedDelay(unit = TimeUnit.NANOSECONDS, await = 10, awaitUnit = TimeUnit.SECONDS)
@@ -73,18 +88,4 @@ final class MethodSchedulerTest {
         }
     }
 
-    @Test
-    void shortRunningTaskShouldBeAllowedToFinish() throws Exception {
-        final MethodSchedulerTest.ShortRun target = new MethodSchedulerTest.ShortRun();
-        TimeUnit.SECONDS.sleep(5);
-        target.close();
-        MatcherAssert.assertThat(target.finished, Matchers.equalTo(true));
-    }
-
-    @Test
-    void interruptLongRunningTask() throws Exception {
-        final MethodSchedulerTest.LongRun target = new MethodSchedulerTest.LongRun();
-        target.close();
-        MatcherAssert.assertThat(target.finished, Matchers.equalTo(false));
-    }
 }
